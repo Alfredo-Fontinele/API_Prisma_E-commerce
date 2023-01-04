0906280df386db838b9431aeda1c5acf46a6732d
Alfredo <alfredofontinele08@gmail.com>
2023-01-04 10:02:08 -0300

    feat : inserting tests

--- src/__tests__/integration/categories/index.test.ts
+++ src/__tests__/integration/categories/index.test.ts
@@ -1,7 +1,6 @@
 import request from "supertest"
 import { app } from "../../../app"
 import { category1 } from './../../mocks/categories/index'
-import { PrismaClient } from '@prisma/client'
 
 describe("POST /categories", () => {
     it("Should be able to create a category", async () => {
